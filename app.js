7617aaf3248ce4a2d54d1258794b151247b28818
Azeem <azeem@gmail.com>
2022-09-20 20:58:35 +0300

    second commit

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8000;
 
-app.use(cors(
-    {
-        origin: '*',
-        credentials: true,            //access-control-allow-credentials:true
-        optionSuccessStatus: 200
-    }
-));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-app.use('/uploads', express.static('uploads')); // accesing image from this folder
-
+app.use("/uploads", express.static("uploads")); // accesing image from this folder
 
 /** import route */
-const adminRoutes = require('./src/routes/admin.route');
-const productRoutes = require('./src/routes/product.route');
-const uploadFileRoutes = require('./src/routes/uploadFile.route');
+const adminRoutes = require("./src/routes/admin.route");
+const productRoutes = require("./src/routes/product.route");
+const uploadFileRoutes = require("./src/routes/uploadFile.route");
 
 /** admin crud */
-app.use('/api/v1/admin', adminRoutes);
+app.use("/api/v1/admin", adminRoutes);
 /** product crud */
-app.use('/api/v1/product', productRoutes);
+app.use("/api/v1/product", productRoutes);
 /** upload file */
-app.use('/api/v1/upload', uploadFileRoutes);
+app.use("/api/v1/upload", uploadFileRoutes);
 
-app.get('/', (req, res) => {
-    res.send('hello..!')
+app.get("/", (req, res) => {
+  res.send("hello..!");
 });
-app.listen(port, () => { console.log(`Express is running in port ${port}`) })
+app.listen(port, () => {
+  console.log(`Express is running in port ${port}`);
+});
